e16a1eba9c6f588c14574dd22ff396de6fee057d
michael <michael.wytock@gmail.com>
2018-07-04 14:08:50 -0400

    Add database seed data.

--- developer/medical_data/ehr-api/db/seeds.rb
+++ developer/medical_data/ehr-api/db/seeds.rb
@@ -1,7 +1,277 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Patient.create([{
+                  id: 293,
+                  first_name: "Derek",
+                  last_name: "Leonard",
+                  dob: 19920317
+                },
+                {
+                  id: 18,
+                  first_name: "Candice",
+                  last_name: "Reilly",
+                  dob: 19830210
+                },
+                {
+                  id: 375,
+                  first_name: "Noreen",
+                  last_name: "Dunbar",
+                  dob: 19760802
+                },
+                {
+                  id: 193,
+                  first_name: "Cecil",
+                  last_name: "Richards",
+                  dob: 19561011
+                }])
+
+Visit.create([{
+                patient_id: 293,
+                date: 20001014,
+                height: 58,
+                weight: 122,
+                ha1c: 4.82,
+                blood_pressure_systolic: 128,
+                blood_pressure_diastolic: 60
+              },
+              {
+                patient_id: 293,
+                date: 20010611,
+                height: 60,
+                weight: 128,
+                ha1c: 4.4,
+                blood_pressure_systolic: 129,
+                blood_pressure_diastolic: 55
+              },
+              {
+                patient_id: 293,
+                date: 20020415,
+                height: 62,
+                weight: 135,
+                ha1c: 4.63,
+                blood_pressure_systolic: 122,
+                blood_pressure_diastolic: 50
+              },
+              {
+                patient_id: 293,
+                date: 20041128,
+                height: 64,
+                weight: 140,
+                ha1c: 4.77,
+                blood_pressure_systolic: 124,
+                blood_pressure_diastolic: 58
+              },
+              {
+                patient_id: 293,
+                date: 20051014,
+                height: 66,
+                weight: 144,
+                ha1c: 4.92,
+                blood_pressure_systolic: 110,
+                blood_pressure_diastolic: 55
+              },
+              {
+                patient_id: 293,
+                date: 20061110,
+                height: 67.4,
+                weight: 146,
+                ha1c: 4.23,
+                blood_pressure_systolic: 115,
+                blood_pressure_diastolic: 62
+              },
+              {
+                patient_id: 293,
+                date: 20080914,
+                height: 67,
+                weight: 148,
+                ha1c: 4.1,
+                blood_pressure_systolic: 120,
+                blood_pressure_diastolic: 63
+              },
+              {
+                patient_id: 293,
+                date: 20100204,
+                height: 67.5,
+                weight: 152,
+                ha1c: 4.05,
+                blood_pressure_systolic: 124,
+                blood_pressure_diastolic: 66
+              },
+              {
+                patient_id: 293,
+                date: 20110115,
+                height: 68,
+                weight: 149,
+                ha1c: 4.8,
+                blood_pressure_systolic: 128,
+                blood_pressure_diastolic: 62
+              },
+              {
+                patient_id: 293,
+                date: 20120403,
+                height: 67,
+                weight: 148,
+                ha1c: 4.28,
+                blood_pressure_systolic: 135,
+                blood_pressure_diastolic: 60
+              },
+              {
+                patient_id: 293,
+                date: 20121002,
+                height: 67.3,
+                weight: 149,
+                ha1c: 4.27,
+                blood_pressure_systolic: 125,
+                blood_pressure_diastolic: 63
+              },
+              {
+                patient_id: 18,
+                date: 20140115,
+                height: 67,
+                weight: 150,
+                ha1c: 4.2,
+                blood_pressure_systolic: 119,
+                blood_pressure_diastolic: 67
+              },
+              {
+                patient_id: 18,
+                date: 20150628,
+                height: 67,
+                weight: 151,
+                ha1c: 5,
+                blood_pressure_systolic: 128,
+                blood_pressure_diastolic: 68
+              },
+              {
+                patient_id: 18,
+                date: 20141015,
+                height: 68,
+                weight: 176,
+                ha1c: 5.6,
+                blood_pressure_systolic: 132,
+                blood_pressure_diastolic: 67
+              },
+              {
+                patient_id: 18,
+                date: 20150112,
+                height: 68.5,
+                weight: 172,
+                ha1c: 5.2,
+                blood_pressure_systolic: 137,
+                blood_pressure_diastolic: 65
+              },
+              {
+                patient_id: 18,
+                date: 20151104,
+                height: 68.2,
+                weight: 177,
+                ha1c: 5.7,
+                blood_pressure_systolic: 138,
+                blood_pressure_diastolic: 67
+              },
+              {
+                patient_id: 375,
+                date: 20161014,
+                height: 68.3,
+                weight: 174,
+                ha1c: 5.8,
+                blood_pressure_systolic: 140,
+                blood_pressure_diastolic: 65
+              },
+              {
+                patient_id: 18,
+                date: 20170412,
+                height: 68.2,
+                weight: 175,
+                ha1c: 5.7,
+                blood_pressure_systolic: 133,
+                blood_pressure_diastolic: 62
+              },
+              {
+                patient_id: 375,
+                date: 20161117,
+                height: 71,
+                weight: 158,
+                ha1c: 4.8,
+                blood_pressure_systolic: 120,
+                blood_pressure_diastolic: 58
+              },
+              {
+                patient_id: 375,
+                date: 20171012,
+                height: "null",
+                weight: "null",
+                ha1c: "null",
+                blood_pressure_systolic: 119,
+                blood_pressure_diastolic: 57
+              },
+              {
+                patient_id: 193,
+                date: 20121015,
+                height: 73,
+                weight: 214,
+                ha1c: 5.9,
+                blood_pressure_systolic: 139,
+                blood_pressure_diastolic: 66
+              },
+              {
+                patient_id: 193,
+                date: 20130604,
+                height: 73.2,
+                weight: 210,
+                ha1c: 6.2,
+                blood_pressure_systolic: 142,
+                blood_pressure_diastolic: 68
+              },
+              {
+                patient_id: 193,
+                date: 20140326,
+                height: 73,
+                weight: 211,
+                ha1c: 6.8,
+                blood_pressure_systolic: 145,
+                blood_pressure_diastolic: 69
+              },
+              {
+                patient_id: 193,
+                date: 20150210,
+                height: 72.8,
+                weight: 214,
+                ha1c: 6.9,
+                blood_pressure_systolic: 144,
+                blood_pressure_diastolic: 70
+              },
+              {
+                patient_id: 193,
+                date: 20151011,
+                height: 72,
+                weight: 210,
+                ha1c: 7,
+                blood_pressure_systolic: 152,
+                blood_pressure_diastolic: 66
+              },
+              {
+                patient_id: 193,
+                date: 20161110,
+                height: 72,
+                weight: 218,
+                ha1c: 7.2,
+                blood_pressure_systolic: 136,
+                blood_pressure_diastolic: 62
+              },
+              {
+                patient_id: 193,
+                date: 20170304,
+                height: 73,
+                weight: 219,
+                ha1c: 7.1,
+                blood_pressure_systolic: 134,
+                blood_pressure_diastolic: 63
+              },
+              {
+                patient_id: 193,
+                date: 20170406,
+                height: 73,
+                weight: 219,
+                ha1c: 7.2,
+                blood_pressure_systolic: 135,
+                blood_pressure_diastolic: 60
+              }])
